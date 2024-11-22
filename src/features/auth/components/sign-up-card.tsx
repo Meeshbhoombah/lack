fedd9047a16b7e59a8c7f3a629721752986f18c3
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-22 12:22:34 -0500

    Added sign-in card featuring

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,8 +1,25 @@
+import { 
+  Card, 
+  CardHeader,
+  CardDescription,
+  CardContent
+} from "@/components/ui/card"
+
+
 export const SignUpCard = () => {
   return (
-    <div>
-      Sign Up Card
-    </div>
+    <Card>
+      <CardHeader className="px-0 pt-0">
+        Login to continue.
+      </CardHeader>
+      <CardDescrption>
+        Use your email or another service to continue.
+      </CardDescrption>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+        </form>
+      </CardContent>
+    </Card>
   )
 }
 
